a6360c3d021ee4044b11a8c7cf4ac529c1d8df88
moud157 <105829245+moud157@users.noreply.github.com>
2024-07-03 09:18:14 -0400

    Update Menu.java

--- src/com/pizzastore/Menu.java
+++ src/com/pizzastore/Menu.java
@@ -3,9 +3,12 @@ package com.pizzastore;
 import java.util.HashMap;
 import java.util.Map;
 
+//Menu class representing the menu of the pizza store
 public class Menu {
+    //Private field to store the menu items and their prices
     private Map<String, Double> menuItems;
 
+    //Constructor that initializes the menu with some default items and their prices
     public Menu() {
         menuItems = new HashMap<>();
         menuItems.put("Pizza", 10.0);
@@ -14,7 +17,9 @@ public class Menu {
         menuItems.put("Salad", 5.0);
     }
 
+    //Get method to return the menu items and their prices
     public Map<String, Double> getMenuItems() {
         return menuItems;
     }
 }
+
